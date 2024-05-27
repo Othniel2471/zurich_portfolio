248417c40193d6aa0c887563aeadca1f69cd6b6f
otsama <othnielomaidu@gmail.com>
2024-05-27 21:22:20 +0100

    implement footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,5 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="w-full py-6 border-t-2 border-gray-100">
+      <div className="flex items-center justify-center gap-1">
+        <p className="text-normal text-sm flex items-center text-white">
+          <FontAwesomeIcon
+            icon={faCopyright}
+            className="mr-1 inline-block h-4 w-4"
+          />
+          Zurich Consulting Limited {currentYear}
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
