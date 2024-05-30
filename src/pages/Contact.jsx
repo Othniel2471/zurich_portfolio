6d254edb6a306324fb2ffcd4f58ca767b3e8d4da
otsama <othnielomaidu@gmail.com>
2024-05-30 13:44:12 +0100

    implement contact page and google map

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,58 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faMapMarkerAlt,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="w-full py-16 md:py-20 2xl:py-24 bg-black text-white">
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 px-4 md:gap-12 md:px-8">
+        <div className="grid place-items-center gap-10 mb-4">
+          <h2 className="capitalize font-[600] text-[30px] text-[#ffff]">
+            contact us
+          </h2>
+          <p className="text-[0.9em] lg:text-[1em] text-base w-[21rem] md:w-[56rem] text-white text-center">
+            Trying to connect with us? Reach us via one of the following
+            channels..
+          </p>
+        </div>
+        <div className="grid place-items-center">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.774265173026!2d7.378334974859375!3d9.084317590979264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e758375323027%3A0xaa9d0dae2032981e!2sDonald%20Building!5e0!3m2!1sen!2sng!4v1717072466765!5m2!1sen!2sng"
+            // width="600"
+            // height="450"
+            // style={{ border: "0" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="w-[300px] h-[200px] lg:w-[1042px] lg:h-[436px] rounded-md"
+          ></iframe>
+        </div>
+        <div className="flex flex-col lg:flex-row justify-between gap-3">
+          <div className="flex gap-1 items-center w-[60%] lg:w-[19%]">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+            <div>
+              Donald building, Nonso Benson Street, Life Camp. Abuja, F.C.T.
+            </div>
+          </div>
+          <div className="flex gap-1 items-center">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <div>
+              <a href="mailto:info@zurichng.com">info@zurichng.com</a>
+            </div>
+          </div>
+          <div className="flex gap-1 items-center">
+            <FontAwesomeIcon icon={faPhoneAlt} />
+            <div>
+              <a href="tel:+234-803-465-2883">+234-803-465-2883</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
